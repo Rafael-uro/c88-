2eab827eacb92adfd228d7807133ca9b99a8165f
Rafael-uro <94479855+Rafael-uro@users.noreply.github.com>
2023-05-12 19:28:58 -0300

    Update CreatePost.js

--- screens/CreatePost.js
+++ screens/CreatePost.js
@@ -41,14 +41,14 @@ export default class CreatePost extends Component {
   }
 
   async addPost() {
-    if (this.state.title && this.state.description && this.state.post && this.state.moral) {
-      var d = new Date()
+    if (this.state.caption) {
+      
       let postData = {
         preview_image: this.state.previewImage,
         caption: this.state.caption,
         description: this.state.description,
        
-        created_on: d.toString(),
+        created_on: new Date(),
         author_uid: firebase.auth().currentUser.uid,
         likes: 0
       }
